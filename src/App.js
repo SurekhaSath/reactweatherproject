2204fef9c86f8c5584fd2e484540b24c7a7443f6
SurekhaSath <surekha.ero@gmail.com>
2024-10-04 12:30:48 -0700

    Crated  basic html page for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./Weather";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <Weather />
+      </div>
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This project is coded by Surekha and is
+          <a href="https://github.com/dashboard"> open sourced in git hub</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+    </div>
+  );
+}
